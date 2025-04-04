0f997c116baff8a9414306bc4f4e7d10b07c61cc
Dana Rose <danarose2021@gmail.com>
2025-04-03 22:20:59 -0400

    Add queuePurgeUsingMessageCounts test case for MockNode using Boundary Value

--- src/test/java/com/github/fridujo/rabbitmq/mock/MockNodeTopDownTest.java
+++ src/test/java/com/github/fridujo/rabbitmq/mock/MockNodeTopDownTest.java
@@ -4,6 +4,8 @@ import com.github.fridujo.rabbitmq.mock.exchange.MockDefaultExchange;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import com.rabbitmq.client.AMQP;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.Collections;
 
@@ -26,5 +28,21 @@ public class MockNodeTopDownTest {
         boolean result = mockNode.basicPublish(exchangeName, routingKey, false, false, props, body);
 
         assertTrue(result, "Message is successfully published to the default exchange");
+    }
+
+    @Test
+    void queuePurgeUsingMessageCounts() {
+        // 0 messages
+        String name = "purge.test.queue";
+        mockNode.queueDeclare(name, false, false, false, Collections.emptyMap());
+        AMQP.Queue.PurgeOk purgeOk = mockNode.queuePurge(name);
+        assertEquals(0, purgeOk.getMessageCount(), "Purge empty queue should return 0 messages removed");
+
+        // 1 message
+        mockNode.queueDeclare(name, false, false, false, Collections.emptyMap());
+        AMQP.BasicProperties props = new AMQP.BasicProperties.Builder().build();
+        mockNode.basicPublish(MockDefaultExchange.NAME, name, false, false, props, "Message".getBytes());
+        purgeOk = mockNode.queuePurge(name);
+        assertEquals(1, purgeOk.getMessageCount(), "Purge 1 message should return 1 message removed");
     }
 }
